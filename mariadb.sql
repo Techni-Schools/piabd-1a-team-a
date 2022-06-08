bc1850e0654699bdc749a8a4150b48cc62705fe1
MikolajMroczkowski <mikolaj.mroczkowski@gmail.com>
2022-06-08 05:38:26 +0200

    fixed ddl

--- mariadb.sql
+++ mariadb.sql
@@ -1,3 +1,7 @@
+CREATE
+DATABASE fridgedb;
+    USE
+fridgedb;
 create table IngredientsCategory
 (
     id   int auto_increment
@@ -10,7 +14,7 @@ create table ingredients
     id       int auto_increment
         primary key,
     name     varchar(100) not null,
-    category int          null,
+    category int null,
     constraint ingredients_IngredientsCategory_id_fk
         foreign key (category) references IngredientsCategory (id)
             on delete set null
@@ -22,7 +26,7 @@ create table users
         primary key,
     username tinytext not null,
     password text     not null,
-    email    text     null,
+    email    text null,
     constraint users_id_uindex
         unique (id),
     constraint users_username_uindex
@@ -35,11 +39,11 @@ create table recipes
 (
     id          int auto_increment
         primary key,
-    name        varchar(255)  not null,
-    userId      int           null,
-    rating      tinyint       null,
-    preptime    int           null,
-    easeOfPrep  varchar(20)   null,
+    name        varchar(255) not null,
+    userId      int null,
+    rating      tinyint null,
+    preptime    int null,
+    easeOfPrep  varchar(20) null,
     photo       varchar(1000) null,
     description varchar(2000) null,
     constraint fk_user_id
